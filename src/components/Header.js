758fd8ee476e69c0916a21a80e1d3c976de7b5be
Cam <cam@cameronfarquharson.ca>
2019-11-25 17:34:31 -0500

    pre-meeting commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,6 @@ import { Link } from 'gatsby'
 const Header = (props) => (
     <header id="header" className="alt">
         <Link to="/" className="logo">KON Logo Here</Link>
-        <nav>
-            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
-        </nav>
     </header>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -48,34 +48,6 @@ class HomeIndex extends React.Component {
                   </header>
                   <Link to="/landing" className="link primary"></Link>
                 </article>
-                <article style={{ backgroundImage: `url(${pic03})` }}>
-                  <header className="major">
-                    <h3>Magna</h3>
-                    <p>Lorem etiam nullam</p>
-                  </header>
-                  <Link to="/landing" className="link primary"></Link>
-                </article>
-                <article style={{ backgroundImage: `url(${pic04})` }}>
-                  <header className="major">
-                    <h3>Ipsum</h3>
-                    <p>Nisl sed aliquam</p>
-                  </header>
-                  <Link to="/landing" className="link primary"></Link>
-                </article>
-                <article style={{ backgroundImage: `url(${pic05})` }}>
-                  <header className="major">
-                    <h3>Consequat</h3>
-                    <p>Ipsum dolor sit amet</p>
-                  </header>
-                  <Link to="/landing" className="link primary"></Link>
-                </article>
-                <article style={{ backgroundImage: `url(${pic06})` }}>
-                  <header className="major">
-                    <h3>Etiam</h3>
-                    <p>Feugiat amet tempus</p>
-                  </header>
-                  <Link to="/landing" className="link primary"></Link>
-                </article>
               </section>
               <section id="two">
                 <div className="inner">
