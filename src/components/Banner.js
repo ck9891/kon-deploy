aff128987c41e96a79eb30464a6035829c87e3b8
Cam <cam@cameronfarquharson.ca>
2019-11-25 18:00:30 -0500

    working on it

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,9 +8,11 @@ const Banner = (props) => (
             </header>
             <div className="content">
                 <p>Your Creditable source for commercial real estate in Hamilton.</p>
+                <p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get in Touch</a></li>
                 </ul>
+                </p>
             </div>
         </div>
     </section>
