67fbef21cec11568591c35e6d2cdbb07f7a31a01
Cam <cam@cameronfarquharson.ca>
2019-11-25 08:11:43 -0500

    initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Home</h1>
+            </header>
+            <div className="content">
+                <p>Your Creditable source for commercial real estate in Hamilton.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get in Touch</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
