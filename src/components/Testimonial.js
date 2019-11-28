1456d7878f579161730d59bc983bbb90070b7acf
Cam <cam@cameronfarquharson.ca>
2019-11-28 08:09:23 -0500

    working on testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Testimonial = (props) => {
 
     return(
-        <div>
+        <div className="testimonial">
             <div className="icon-holder">
-
+                <FontAwesomeIcon icon={faUser} />
             </div>
             <div className="testimonial-body">
                 <blockquote>{props.text}</blockquote>
