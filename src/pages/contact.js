34d4823ffe176416586d454e16fbae04da55840f
Cam <cam@cameronfarquharson.ca>
2020-01-20 08:22:10 -0500

    almost got form going

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,35 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Helmet from 'react-helmet'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faUserFriends,
-  faCoins,
-  faFileSignature,
-  faMoneyBill
-} from '@fortawesome/free-solid-svg-icons'
-// library.add(faEnvelope, faKey);
+
 
 
 import Layout from '../components/layout'
-import Banner from '../components/Banner'
 
-import servicesPic from '../assets/images/services.jpg'
 import creaLogo from '../assets/images/logo_crea.svg'
-import oreaLogo from '../assets/images/logo-orea.svg'
 import hdaaLogo from '../assets/images/logo-hdaa.gif'
 import Affiliate from '../components/Affiliate';
 
+const CONTACT_MUTATION = gql`
+  mutation CreateSubmissionMutation($clientMutationId: String!, $fullName: String!, $email: String!, $message: String!) {
+    createSubmission( input: {
+      clientMutationId: $clientMutationId,
+      fullName: $fullName,
+      email: $email,
+      message: $message
+    } ) {
+      success
+      data
+    }
+  }
+`
+
 const HomeIndex = ({data}) => {
-        const affiliates = [
-          creaLogo,
-          // oreaLogo,
-          hdaaLogo,
-          data.logo4.childImageSharp.fluid.src,
-          data.logo5.childImageSharp.fluid.src,
-        ]
+        
+        const [fullNameValue, setFullNameValue] = useState('')
+        const [emailValue, setEmailValue] = useState('')
+        const [messageValue, setMessageValue] = useState('')
+
+
         return (
+          
           <Layout>
             <Helmet
               title="KV Realty"
@@ -44,24 +50,60 @@ const HomeIndex = ({data}) => {
               <section>
                 <div className="inner" id="contactUs">
                     <h2>Contact Us</h2>
-                    <form id="contactForm">
+                    
+                    <Mutation mutation={CONTACT_MUTATION}>
+                      { (createSubmission, {loading, error, data}) => (
+
+                     <>
+                    <form id="contactForm" onSubmit={
+                      async event => {
+                        event.preventDefault()
+                        createSubmission({
+                          variables: {
+                            clientMutationId: 'example',
+                            fullName: fullNameValue,
+                           
+                            email: emailValue,
+                            message: messageValue,
+                          }
+                        })
+                      }
+                    }>
                         <label for="fullName">
                             Your Name
-                            <input type="text" name="fullName" placeholder="John Doe" />
+                            <input type="text" name="fullName" placeholder="John Doe" value={fullNameValue} 
+                              onChange={event => {setFullNameValue(event.target.value)}}
+                            />
                         </label>
                         <label for="email">
                             Your Email
-                            <input type="email" name="email" placeholder="abc@abc.com" />
+                            <input type="email" name="email" placeholder="abc@abc.com" value={emailValue}
+                              onChange = {
+                                event => {
+                                  setEmailValue(event.target.value)
+                                }
+                              }
+                            />
                         </label>
                         <label for="message">Message
-                            <textarea name="message"></textarea>
+                            <textarea name="message" onChange={event => {setMessageValue(event.target.value)}}>{messageValue}</textarea>
                         </label>
                         <button id="getInTouch" type="submit">Get in Touch</button>
                     </form>
+                      <div style={{padding: '2rem'}}>
+                        {loading && <p>loading...</p>}
+                        {error && <p>An unknown error has occurred, please try again later</p>}
+                        {data && <p>Message successfully sent!</p>}
+                      </div>
+                      </>
+                     )}
+                    </Mutation>
+                    
                 </div>
                </section>
             </main>
           </Layout>
+          
         )
     
 }
